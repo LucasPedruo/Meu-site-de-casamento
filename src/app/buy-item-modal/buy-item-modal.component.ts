3d54777ee8e905ee7979423c778077fe5c9de205
NeetoL <97066149+NeetoL@users.noreply.github.com>
2025-01-13 01:37:14 -0300

    ajustes modal

--- src/app/buy-item-modal/buy-item-modal.component.ts
+++ src/app/buy-item-modal/buy-item-modal.component.ts
@@ -18,64 +18,219 @@ import { FormsModule } from '@angular/forms';
       </div>
 
       <div *ngIf="!paymentSuccess">
-        <h2>Confirmar Compra</h2>
+        <h2 class="modal-title">Confirmar Compra</h2>
         
-        <div *ngIf="!paymentDataSubmitted">
-          <h3>Informe seus dados para continuar</h3>
-          <form (ngSubmit)="onSubmit()" #paymentForm="ngForm">
-            <label for="name">Nome</label>
-            <input type="text" id="name" name="name" [(ngModel)]="userName" required />
-            <br />
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email" [(ngModel)]="userEmail" required />
-            <br />
-            <button type="submit" mat-button [disabled]="!paymentForm.valid">Enviar</button>
+        <div *ngIf="!paymentDataSubmitted" class="form-container">
+          <h3 class="form-title">Informe seus dados para continuar</h3>
+          <form (ngSubmit)="onSubmit()" #paymentForm="ngForm" class="payment-form">
+            <div class="form-group">
+              <label for="name">Nome</label>
+              <input type="text" id="name" name="name" [(ngModel)]="userName" required />
+            </div>
+            <div class="form-group">
+              <label for="email">Email</label>
+              <input type="email" id="email" name="email" [(ngModel)]="userEmail" required />
+            </div>
+            <button type="submit" mat-button [disabled]="!paymentForm.valid" class="submit-button">
+              Enviar
+            </button>
           </form>
         </div>
 
-        <div *ngIf="paymentDataSubmitted && pixUrl">
-          <h3>Pagamento via PIX</h3>
-          <p>Use o QR code abaixo para pagar:</p>
-          <img [src]="pixUrl" alt="QR Code Pix" width="200">
+        <!-- Tela de carregamento enquanto o PIX não é gerado -->
+        <div *ngIf="paymentDataSubmitted && !pixUrl" class="loading-container">
+          <p>Gerando QR Code do pagamento...</p>
         </div>
 
-        <div *ngIf="paymentDataSubmitted && !pixUrl">
+        <!-- Quando o PIX QR code estiver disponível -->
+        <div *ngIf="paymentDataSubmitted && pixUrl" class="pix-container">
+          <h3>Pagamento via PIX</h3>
+          <p>Use o QR code abaixo para pagar:</p>
+          <img [src]="pixUrl" alt="QR Code Pix" width="200" />
+          <div class="copy-container">
+            <input 
+              type="text" 
+              readonly 
+              [value]="pixcec" 
+              class="copy-input"
+            />
+            <button mat-button (click)="copyQRCode()" class="copy-button">
+              Copiar
+            </button>
+          </div>
+        </div>
+
+        <div *ngIf="paymentDataSubmitted && !pixUrl" class="mercadopago-container">
           <h3>Pagamento via Mercado Pago</h3>
-          <button mat-button (click)="redirectToPayment()">Pagar agora</button>
         </div>
       </div>
 
       <div class="actions" *ngIf="!paymentSuccess">
-        <button mat-button (click)="redirectToSandbox()">Pagar de outra forma</button>
-        <button mat-button (click)="closeModal()">Cancelar</button>
+        <button 
+          mat-button 
+          *ngIf="!paymentDataSubmitted" 
+          (click)="closeModal()" 
+          class="cancel-button"
+        >
+          Cancelar
+        </button>
+        <button 
+          mat-button 
+          *ngIf="pixUrl" 
+          (click)="redirectToSandbox()" 
+          class="alternate-payment-button"
+        >
+          Pagar de outra forma
+        </button>
       </div>
     </div>
   `,
   styles: [
     `
       .modal-content {
-        text-align: center;
+        border-radius: 8px;
         padding: 20px;
+        max-width: 100%;
+        margin: auto;
+        text-align: center;
       }
+
+      .modal-title {
+        font-size: 1.8rem;
+        margin-bottom: 15px;
+        color: #333;
+      }
+
+      .form-container {
+        text-align: left;
+      }
+
+      .form-title {
+        font-size: 1.2rem;
+        margin-bottom: 10px;
+        color: #555;
+      }
+
+      .form-group {
+        margin-bottom: 15px;
+      }
+
+      .form-group label {
+        display: block;
+        font-size: 0.9rem;
+        color: #555;
+        margin-bottom: 5px;
+      }
+
+      .form-group input {
+        width: 100%;
+        padding: 8px;
+        border: 1px solid #ddd;
+        border-radius: 4px;
+        font-size: 1rem;
+      }
+
+      .submit-button {
+        width: 100%;
+        padding: 10px;
+        background-color: #1976d2;
+        color: #fff;
+        border: none;
+        border-radius: 4px;
+        font-size: 1rem;
+        cursor: pointer;
+      }
+
+      .submit-button[disabled] {
+        background-color: #aaa;
+        cursor: not-allowed;
+      }
+
+      .pix-container,
+      .mercadopago-container {
+        margin-top: 15px;
+        text-align: center;
+      }
+
+      .pix-container img {
+        margin-top: 10px;
+        border: 1px solid #ddd;
+        border-radius: 8px;
+      }
+
+      .loading-container {
+        margin-top: 15px;
+        color: #1976d2;
+        font-size: 1.2rem;
+        font-weight: bold;
+      }
+
+      .actions {
+        margin-top: 20px;
+        display: flex;
+        justify-content: space-between;
+        gap: 10px;
+      }
+
+      .cancel-button {
+        background-color: #f44336;
+        color: #fff;
+        padding: 10px 15px;
+        border-radius: 4px;
+        font-size: 1rem;
+        cursor: pointer;
+        border: none;
+      }
+
+      .alternate-payment-button {
+        background-color: #00796b;
+        color: #fff;
+        border: none;
+        padding: 10px 15px;
+        border-radius: 4px;
+        font-size: 1rem;
+        cursor: pointer;
+        margin-left: 50px;
+      }
+
       .success-content {
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center;
       }
+
       .success-icon {
         font-size: 80px;
-        color: green;
+        color: #4caf50;
         margin-bottom: 10px;
       }
-      .actions {
+
+      .copy-container {
         margin-top: 10px;
         display: flex;
         justify-content: center;
-        gap: 10px;
+        align-items: center;
+        width: 100%;
       }
-      img {
-        margin-top: 15px;
+
+      .copy-input {
+        width: 80%;
+        padding: 8px;
+        font-size: 1rem;
+        border: 1px solid #ddd;
+        border-radius: 4px;
+        margin-right: 10px;
+      }
+
+      .copy-button {
+        background-color: #1976d2;
+        color: #fff;
+        padding: 8px 12px;
+        font-size: 1rem;
+        border-radius: 4px;
+        cursor: pointer;
+        min-width: 100px;
       }
     `,
   ],
@@ -90,6 +245,7 @@ export class BuyItemModalComponent implements OnInit {
   errorMessage: string = '';
   paymentId: string = '';
   paymentSuccess: boolean = false;
+  pixcec: string = '';
 
   constructor(
     public dialogRef: MatDialogRef<BuyItemModalComponent>,
@@ -126,6 +282,7 @@ export class BuyItemModalComponent implements OnInit {
         this.paymentUrl = data.checkout_init_point;
         this.pixUrl = 'data:image/png;base64,' + data.pix_qr_code_base64;
         this.paymentId = data.full_info_for_developer.pix.id;
+        this.pixcec = data.pix_qr_code;
         this.startPaymentStatusCheck();
       })
       .catch((error) => {
@@ -165,11 +322,24 @@ export class BuyItemModalComponent implements OnInit {
 
   redirectToSandbox(): void {
     if (this.paymentUrl) {
-      const sandboxUrl = this.paymentUrl.replace('https://www.mercadopago.com.br', 'https://sandbox.mercadopago.com.br');
+      const sandboxUrl = this.paymentUrl.replace(
+        'https://www.mercadopago.com.br',
+        'https://sandbox.mercadopago.com.br'
+      );
       window.location.href = sandboxUrl;
     } else {
       console.error('URL de pagamento não encontrada');
     }
+  }
+
+  copyQRCode(): void {
+    const copyText = document.createElement('input');
+    copyText.value = this.pixUrl;
+    document.body.appendChild(copyText);
+    copyText.select();
+    document.execCommand('copy');
+    document.body.removeChild(copyText);
+    alert('QR Code copiado para a área de transferência');
   }
 
   closeModal(): void {
--- src/app/lista-de-presentes/lista-de-presentes.component.ts
+++ src/app/lista-de-presentes/lista-de-presentes.component.ts
@@ -16,38 +16,41 @@ export class ListaDePresentesComponent {
 
   popupVisible = false;
 
-  items = [
-    { id: 1, title: 'Sofá', category: 'casa', price: 1500, image: 'sofa.jpg', status: 'disponivel' },
-    { id: 2, title: 'Viagem para Paris', category: 'lua_de_mel', price: 10000, image: 'paris.jpg', status: 'disponivel' },
-    { id: 3, title: 'Jogo de Panelas', category: 'casa', price: 500, image: 'panelas.jpg', status: 'disponivel' },
-    { id: 4, title: 'Quadro Moderno', category: 'criativos', price: 300, image: 'quadro.jpg', status: 'disponivel' },
-    { id: 5, title: 'Mesa de Jantar', category: 'casa', price: 2500, image: 'mesa.jpg', status: 'disponivel' },
-    { id: 6, title: 'Cafeteira Elétrica', category: 'cozinha', price: 350, image: 'cafeteira.jpg', status: 'disponivel' },
-    { id: 7, title: 'Tênis Esportivo', category: 'esporte', price: 450, image: 'tenis.jpg', status: 'indisponivel' },
-    { id: 8, title: 'Smartphone', category: 'tecnologia', price: 3000, image: 'smartphone.jpg', status: 'disponivel' },
-    { id: 9, title: 'Livro de Romance', category: 'livros', price: 40, image: 'livro.jpg', status: 'disponivel' },
-    { id: 10, title: 'Fones de Ouvido', category: 'tecnologia', price: 200, image: 'fones.jpg', status: 'disponivel' },
-    { id: 11, title: 'Relógio de Parede', category: 'casa', price: 120, image: 'relogio.jpg', status: 'disponivel' },
-    { id: 12, title: 'Bicicleta', category: 'esporte', price: 1200, image: 'bicicleta.jpg', status: 'indisponivel' },
-    { id: 13, title: 'Jogo de Cama', category: 'casa', price: 180, image: 'cama.jpg', status: 'disponivel' },
-    { id: 14, title: 'Óculos de Sol', category: 'moda', price: 600, image: 'oculos.jpg', status: 'disponivel' },
-    { id: 15, title: 'Tablet', category: 'tecnologia', price: 2500, image: 'tablet.jpg', status: 'indisponivel' },
-    { id: 16, title: 'Garrafa Térmica', category: 'cozinha', price: 80, image: 'garrafa.jpg', status: 'disponivel' },
-    { id: 17, title: 'Patins In-Line', category: 'esporte', price: 700, image: 'patins.jpg', status: 'disponivel' },
-    { id: 18, title: 'Monitor 4K', category: 'tecnologia', price: 2000, image: 'monitor.jpg', status: 'disponivel' },
-    { id: 19, title: 'Câmera Fotográfica', category: 'tecnologia', price: 3500, image: 'camera.jpg', status: 'indisponivel' },
-    { id: 20, title: 'Abajur de Mesa', category: 'casa', price: 150, image: 'abajur.jpg', status: 'disponivel' },
-    { id: 21, title: 'Cadeira Gamer', category: 'tecnologia', price: 1200, image: 'cadeira.jpg', status: 'disponivel' },
-    { id: 22, title: 'Mochila Executiva', category: 'moda', price: 300, image: 'mochila.jpg', status: 'disponivel' },
-    { id: 23, title: 'Churrasqueira Portátil', category: 'casa', price: 500, image: 'churrasqueira.jpg', status: 'disponivel' },
-    { id: 24, title: 'Piscina Inflável', category: 'casa', price: 1000, image: 'piscina.jpg', status: 'indisponivel' },
-    { id: 25, title: 'Kit de Ferramentas', category: 'casa', price: 250, image: 'ferramentas.jpg', status: 'disponivel' },
-    { id: 26, title: 'Batedeira Planetária', category: 'cozinha', price: 400, image: 'batedeira.jpg', status: 'disponivel' },
-    { id: 27, title: 'Carrinho de Bebê', category: 'infantil', price: 900, image: 'carrinho.jpg', status: 'disponivel' },
-    { id: 28, title: 'Boneca de Pano', category: 'infantil', price: 100, image: 'boneca.jpg', status: 'disponivel' },
-    { id: 29, title: 'Suporte para TV', category: 'casa', price: 150, image: 'suporte.jpg', status: 'disponivel' },
-    { id: 30, title: 'Climatizador de Ar', category: 'tecnologia', price: 800, image: 'climatizador.jpg', status: 'indisponivel' }
+ items = [
+    { id: 1, title: 'Sofá', category: 'casa', price: 1500, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 2, title: 'Viagem para Paris', category: 'lua_de_mel', price: 10000, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 3, title: 'Jogo de Panelas', category: 'casa', price: 500, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 4, title: 'Quadro Moderno', category: 'criativos', price: 300, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 5, title: 'Mesa de Jantar', category: 'casa', price: 2500, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 6, title: 'Cafeteira Elétrica', category: 'cozinha', price: 350, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 7, title: 'Tênis Esportivo', category: 'esporte', price: 450, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'indisponivel' },
+    { id: 8, title: 'Smartphone', category: 'tecnologia', price: 3000, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 9, title: 'Livro de Romance', category: 'livros', price: 40, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 10, title: 'Fones de Ouvido', category: 'tecnologia', price: 200, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 11, title: 'Relógio de Parede', category: 'casa', price: 120, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 12, title: 'Bicicleta', category: 'esporte', price: 1200, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'indisponivel' },
+    { id: 13, title: 'Jogo de Cama', category: 'casa', price: 180, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 14, title: 'Óculos de Sol', category: 'moda', price: 600, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 15, title: 'Tablet', category: 'tecnologia', price: 2500, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'indisponivel' },
+    { id: 16, title: 'Garrafa Térmica', category: 'cozinha', price: 80, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 17, title: 'Patins In-Line', category: 'esporte', price: 700, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 18, title: 'Monitor 4K', category: 'tecnologia', price: 2000, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 19, title: 'Câmera Fotográfica', category: 'tecnologia', price: 3500, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'indisponivel' },
+    { id: 20, title: 'Abajur de Mesa', category: 'casa', price: 150, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 21, title: 'Cadeira Gamer', category: 'tecnologia', price: 1200, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 22, title: 'Mochila Executiva', category: 'moda', price: 300, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 23, title: 'Churrasqueira Portátil', category: 'casa', price: 500, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 24, title: 'Piscina Inflável', category: 'casa', price: 1000, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'indisponivel' },
+    { id: 25, title: 'Kit de Ferramentas', category: 'casa', price: 250, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 26, title: 'Batedeira Planetária', category: 'cozinha', price: 400, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 27, title: 'Carrinho de Bebê', category: 'infantil', price: 900, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 28, title: 'Boneca de Pano', category: 'infantil', price: 100, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 29, title: 'Suporte para TV', category: 'casa', price: 150, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'disponivel' },
+    { id: 30, title: 'Climatizador de Ar', category: 'tecnologia', price: 800, image: 'https://ellycandido.com.br/wp-content/uploads/2024/08/Como-funciona-um-casamento-na-praia-1024x683.jpg', status: 'indisponivel' }
   ];
+  
+  
+  
 
   buttonActive: string = 'all';
 
