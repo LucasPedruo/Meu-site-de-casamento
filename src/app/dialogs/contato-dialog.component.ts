9d2ffad33380651903fee81e5a3cabc441d0aead
Lucas Pedro <147441250+LucasPedruo@users.noreply.github.com>
2025-04-21 14:57:08 -0300

    .

--- src/app/dialogs/contato-dialog.component.ts
+++ src/app/dialogs/contato-dialog.component.ts
@@ -11,8 +11,8 @@ import { MatButtonModule } from '@angular/material/button';
     <h2 mat-dialog-title>Contato</h2>
     <mat-dialog-content>
       <p>Para entrar em contato conosco, por favor envie um e-mail para:</p>
-      <p><strong>aaaaaaaaaaa</strong></p>
-      <p>Ou entre em contato pelo WhatsApp: <strong>(11) 99999-9999</strong></p>
+      <p><strong>lucasdearaujopedrolap@gmail.com</strong></p>
+      <p>Ou entre em contato pelo WhatsApp: <strong>(21) 988460135</strong></p>
     </mat-dialog-content>
     <mat-dialog-actions align="end">
       <button mat-button [mat-dialog-close]="true">Fechar</button>
